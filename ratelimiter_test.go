3c88eea334435e732be5991df12934dd416ca562
petrolax <poa21y@gmail.com>
2021-08-17 13:44:20 +0300

    Added test

---
+++ ratelimiter_test.go
@@ -0,0 +1,28 @@
+package ratelimiter
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+)
+
+func TestRateLimiter(t *testing.T) {
+	intch := make(chan int)
+	rt := NewRateLimiter(intch)
+
+	go func() {
+		var wg sync.WaitGroup
+
+		for i := 0; i < 10000; i++ {
+			wg.Add(1)
+			go func(v int, wg *sync.WaitGroup) {
+				intch <- v
+				wg.Done()
+			}(i, &wg)
+		}
+		wg.Wait()
+		close(intch)
+	}()
+
+	fmt.Println(rt.Run())
+}
